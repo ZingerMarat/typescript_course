cf65467b17ea39dd8999dcb79b6f5942872b2949
Zinger Marat <zingermarat@gmail.com>
2026-02-07 21:23:19 +0200

    Add interfaces for mobile phones and implement filtering function by manufacture date

---
+++ step_8/index (1).ts
@@ -0,0 +1,70 @@
+interface IPhone {
+  company: string
+  number: number
+}
+
+// IMobilePhone должен наследоваться от IPhone,
+// тип свойства companyPartner зависит от свойства company
+
+interface IMobilePhone extends IPhone {
+  size: string
+  companyPartner: IPhone["company"]
+  manufactured: Date
+}
+
+// Типизировать объект phones
+
+const phones: IMobilePhone[] = [
+  {
+    company: "Nokia",
+    number: 1285637,
+    size: "5.5",
+    companyPartner: "MobileNokia",
+    manufactured: new Date("2022-09-01"),
+  },
+  {
+    company: "Samsung",
+    number: 4356637,
+    size: "5.0",
+    companyPartner: "SamMobile",
+    manufactured: new Date("2021-11-05"),
+  },
+  {
+    company: "Apple",
+    number: 4552833,
+    size: "5.7",
+    companyPartner: "no data",
+    manufactured: new Date("2022-05-24T12:00:00"),
+  },
+]
+
+interface IPhonesManufacturedAfterDate extends IMobilePhone {
+  initialDate: string
+}
+
+// Функция должна отфильтровать массив данных и вернуть новый массив
+// с телефонами, выпущенными после даты в третьем аргументе
+
+function filterPhonesByDate<T extends IMobilePhone>(
+  phones: T[],
+  key: keyof T,
+  initial: string
+): Partial<IPhonesManufacturedAfterDate>[] {
+  const initialDate: Date = new Date(initial)
+
+  return phones
+    .filter((p) => {
+      const manufactured = p[key]
+
+      return manufactured instanceof Date && manufactured > initialDate
+    })
+    .map((p) => {
+      const newObj = { ...p, initialDate: initial }
+      return newObj
+    })
+}
+
+// Второй аргумент при вызове функции должен быть связан с первым,
+// а значит мы получим подсказки - свойства этого объекта
+
+console.log(filterPhonesByDate(phones, "manufactured", "2022-01-01"))
